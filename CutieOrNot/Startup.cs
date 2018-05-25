405fb0192b7f381361977dfa11900e0182850509
Jim Kwok <jimkwok101@gmail.com>
2018-05-24 20:58:37 -0400

    Add project files.

---
+++ CutieOrNot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CutieOrNot.Startup))]
+namespace CutieOrNot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
